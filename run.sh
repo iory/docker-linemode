597eef711c0334212e065b641c1c5df1ab6c7e9e
iory <ab.ioryz@gmail.com>
2018-08-16 07:56:28 +0900

    Modified run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+xhost +local:root
 docker run -it --rm \
        --net=host \
+       -e DISPLAY=$DISPLAY \
+       -e QT_X11_NO_MITSHM=1 \
+       -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
        iory/linemod \
        /bin/bash
+xhost -local:
